172778468bc944e0eb2d41f85fd564edf4cb7c56
Samsursekh <sekhsamsur44@gmail.com>
2024-02-17 22:29:18 +0530

    some new changes added

--- src/Components/Lamp.jsx
+++ src/Components/Lamp.jsx
@@ -16,7 +16,7 @@ export function Lamp() {
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        Build lamps <br /> the right way
+        Turn prospects <br /> into loyal customer
       </motion.h1>
     </LampContainer>
   );
